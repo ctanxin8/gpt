20c28940822176f5ec7030baec52a56e1a0ad92d
ctanxin8 <133303086+ctanxin8@users.noreply.github.com>
2023-06-07 18:19:09 +0800

    莘羽子

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
