bced5eb80c50d68a66ed09182e23cc99d1cad534
ctanxin8 <133303086+ctanxin8@users.noreply.github.com>
2023-06-07 19:51:03 +0800

    chatgpt

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
